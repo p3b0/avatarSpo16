d48832fa6bf5be6dc93e7bdfc423ed307e027ccf
Americo Perez <americo@telia.com>
2017-02-15 14:47:13 +0100

    CRUD på plats, updated CSS,  index.html, .aspx

--- citatCard.aspx
+++ citatCard.aspx
@@ -1,21 +1,64 @@
 <%@ Page Language="C#" masterpagefile="~masterurl/custom.master" title="Untitled 1" inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=16.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" meta:progid="SharePoint.WebPartPage.Document" %>
 <%@ Register tagprefix="SharePoint" namespace="Microsoft.SharePoint.WebControls" assembly="Microsoft.SharePoint, Version=16.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
 <asp:Content id="Content2" runat="Server" contentplaceholderid="PlaceHolderAdditionalPageHead">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/custom.css">
 </asp:Content>
 
 <asp:Content id="Content1" runat="Server" contentplaceholderid="PlaceHolderMain">
     <div id="citat-card">
 		<header>
+			<span id="expand" class="glyphicon glyphicon-resize-full" aria-hidden="true"></span>
 			<img src="" alt="">
+			
 			<h1></h1>
 			<h2></h2>
 		</header>
 		<div id="citat">
-			<p></p>
+			<p> </p>
+			<span class="hidden"></span>
 		</div>
+		<hr>
+		<div id="admin">
+			<div id="controls" class="text-center">
+				<button id="editButton" class="btn btn-info">Edit</button>
+				<button id="newButton" class="btn btn-warning">Create New</button>
+				<button id="deleteItem" class="btn btn-danger">Delete</button>
+			</div>
+			
+
+			<div id="editItem">
+				<h4>Edit User</h4>
+				<label for="name">Name</label>
+				<input type="text" id="name" required>
+				<label for="lastname">Lastname</label>
+				<input type="text" id="lastname" required>
+				<label for="course">Course</label>
+				<input type="text" id="course" required>
+				<label for="citat">Citat</label>
+				<textarea name="citat" id="citat" cols="39" rows="5" required></textarea>
+				<button class="btn btn-success" id="update">Update</button>
+			</div>
+			<div id="newItem">
+				<h4>Create User</h4>
+				<label for="name">Name</label>
+				<input type="text" id="name" required>
+				<label for="lastname">Lastname</label>
+				<input type="text" id="lastname" required>
+				<label for="course">Course</label>
+				<input type="text" id="course" required>
+				<label for="email">Email</label>
+				<input type="email" id="email" required>
+				<label for="citat">Citat</label>
+				<textarea name="citat" id="citat" cols="39" rows="5"></textarea>
+				<label for="image">Image</label>
+				<input type="file" id="image" accept="image/jpeg, image/png">
+				<button class="btn btn-success" id="create">Create</button>
+			</div>
+		</div>
+		
 	</div>
-    <!--Testkommentar-->
+
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 	<script src="js/custom.js"></script>
 </asp:Content>
--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,9 @@
 (function ($, window, document) {
     $(function () {
+        var citatHeader = $('#citat-card header');
+        var eID = '';
+
+        /* SHOW CARD */
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -13,27 +17,187 @@
 
         $.ajax(settings).done(function (data) {
 
-            var random = makeUniqueRandom(); // Get random number without repeating the last one
+            var random = makeUniqueRandom(data); // Get random number without repeating the last one
 
 
             $('#citat-card').on('click', function () {
                 $(this).addClass('show');
             });
-            var citatHeader = $('#citat-card header');
+            //var citatHeader = $('#citat-card header');
 
             citatHeader.find('img').attr("src", "https://employees-99bd.restdb.io/media/" + data[random].bilder);
             citatHeader.find('h1').text(data[random].fnamn + ' ' + data[random].enamn);
             citatHeader.find('h2').text(data[random].klassnamn);
             $('#citat-card #citat p').text(data[random].citat);
-
+            $('#citat-card .hidden').text(data[random]._id);
+            eID = data[random].eID;
+            console.log('This eID: ' + eID);
         }).fail(function () {
             console.log('error');
         });
+        /* END SHOW CARD */
+
+        /* DELETE */
+        $('#deleteItem').click(function () {
+
+            var id = $('.hidden').text();
+            var settings2 = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://employees-99bd.restdb.io/rest/employees/" + id,
+                "method": "DELETE",
+                "headers": {
+                    "content-type": "application/json",
+                    "x-apikey": "589c6e2e64c380c04d1ed801",
+                    "cache-control": "no-cache"
+                }
+            }
+
+            $.ajax(settings2).done(function (response) {
+                location.reload();
+
+            }).fail(function () {
+                console.log('DELETE error');
+            });
+        });
+        // END DELETE
+
+        /* EDIT */
+        $('#editButton').on('click', function (e) {
+            $('#newItem').css("display", "none");
+            $('#editItem').toggle();
+            var id = $('.hidden').text();
+
+            var name = citatHeader.find('h1').text();
+            var firstname = name.split(" ")[0];
+            var lastname = name.split(" ")[1];
+            var classname = citatHeader.find('h2').text();
+            var citate = $('#citat-card #citat p').text();
+
+            $('#editItem input#name').val(firstname);
+            $('#editItem input#lastname').val(lastname);
+            $('#editItem input#course').val(classname);
+            $('#editItem textarea#citat').val(citate);
+
+
+
+            e.stopPropagation();
+
+            $('#update').on('click', function (e) {
+                var newFirstName = $('#editItem input#name').val();
+                var newLastName = $('#editItem input#lastname').val();
+                var newClassName = $('#editItem input#course').val();
+                var newCitat = $('#editItem textarea#citat').val();
+                var thisEID = eID;
+
+                var jsondata = {
+                    "eID": thisEID,
+                    "fnamn": newFirstName,
+                    "enamn": newLastName,
+                    "klassnamn": newClassName,
+                    "citat": newCitat
+                };
+                console.log(jsondata);
+                var settings3 = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://employees-99bd.restdb.io/rest/employees/" + id,
+                    "method": "PUT",
+                    "headers": {
+                        "content-type": "application/json",
+                        "x-apikey": "589c6e2e64c380c04d1ed801",
+                        "cache-control": "no-cache"
+                    },
+                    "processData": false,
+                    "data": JSON.stringify(jsondata)
+                }
+
+                $.ajax(settings3).done(function (response) {
+                    location.reload();
+                }).fail(function (error) {
+                    console.log(error);
+                });
+            });
+
+        });
+        // END EDIT
+
+
+
+        /* NEW ITEM */
+        $('#newButton').on('click', function (e) {
+            $('#editItem').css("display", "none");
+            $('#newItem').toggle();
+
+            e.stopPropagation();
+
+            $('#create').on('click', function () {
+                var newItem = $('#newItem');
+                var firstname = newItem.find('#name').val();
+                var lastname = newItem.find('#lastname').val();
+                var email = newItem.find('#email').val();
+                var classname = newItem.find('#course').val();
+                var citate = newItem.find('#citat').val();
+
+                var formData = new FormData();
+                var image = $('#image')[0].files;
+
+                var jsondata = {
+                    "fnamn": firstname,
+                    "enamn": lastname,
+                    "klassnamn": classname,
+                    "e-mail": email,
+                    "citat": citate
+                };
+
+
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://employees-99bd.restdb.io/rest/employees",
+                    "method": "POST",
+                    "headers": {
+                        "content-type": "application/json",
+                        "x-apikey": "589c6e2e64c380c04d1ed801",
+                        "cache-control": "no-cache"
+                    },
+                    "processData": false,
+                    "data": JSON.stringify(jsondata)
+                }
+
+                $.ajax(settings).done(function (response) {
+                    location.reload();
+                });
+
+                // Setup for uploading images (See more: https://restdb.io/blog/570d28ae385af21e000000af)
+                // I get "illegal invocation" error in the console comming from jQuery.
+
+                /*for(var i= 0; i < image.length; i++) {
+					var file = image[i];
+					var name = image[0].name;
+					formData.append('myImage', file, file.name);
+				}
+				
+				$.ajax({
+					"data": formData,
+					"url": "https://employees-99bd.restdb.io/media/",
+					"method": "POST",
+					"contentType": false,
+				}).done(function(response){
+					console.log(response);
+				});*/
+            });
+
+        });
+        // END NEW ITEM
+
     });
 
     var uniqueRandoms = [];
-    var numRandoms = 7; // number of elements in the array
-    function makeUniqueRandom() {
+    var numRandoms = ''; // number of elements in the array
+    function makeUniqueRandom(data) {
+        numRandoms = data.length;
+
         if (!uniqueRandoms.length) { // when uniqueRandoms[] is empty
             for (var i = 0; i < numRandoms; i++) {
                 uniqueRandoms.push(i); // populate uniqueRandoms[] with new values
@@ -51,7 +215,3 @@
     }
 
 }(window.jQuery, window, document));
-
-
-
-
